762223e55618e21f01b9167b6248690d912fcaf3
JoesphAguilar <josephjd93@gmail.com>
2022-03-24 23:12:57 -0700

    Restarted code from scratch; Working on Character.js and added characterdetails.js file to component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from "react"
+
+const Character = props => {
+    const { charInfo } = props 
+
+    return (
+        <h2>{charInfo.name}</h2>
+    )
+}
+
+export default Character;
