c76f01c8131c04729250f3df90df700079b8542f
JoesphAguilar <josephjd93@gmail.com>
2022-03-24 23:47:57 -0700

    added useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
+import './App.css'
 
+import Character from './components/Character';
+import CharacterDetails from './components/CharacterDetails';
 
 const App = () => {
+
+  const [starChar, setStarChar] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      console.log(res.data.results);
+    })
+    .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="App">
